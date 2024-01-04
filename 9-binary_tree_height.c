061032d80512a56541e44c41182639faca4fe2be
vSolomony <mohamed.a.kassem95@gmail.com>
2024-01-03 20:58:35 -0800

    123

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height) + 1;
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
